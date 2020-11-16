05277d20913ccd57e01b2e2b07ad9de50bfef418
Zac Farnworth <zacharyfarnworth@gmail.com>
2020-11-15 22:46:16 -0700

    removing main folder from repo

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# Password Generator
+Password Generator created by Zachary Farnworth. This is a challege to build a random password generator using Javascript. The password generator includes features that allow users to generate a password using lower case letters, upper case letters, numbers, and symbols. 
+
+
+## Useful links
+* Github Repository: https://github.com/ZFarnworth/password_generator
+
+* Live-repo: https://zfarnworth.github.io/password_generator
+
+## Contact
+* Email: zacharyfarnworth@gmail.com
---
+++ assets/js/script.js
@@ -0,0 +1,89 @@
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+//Page state when loaded
+var length = '';
+var isLower = false;
+var isUpper = false;
+var isNumbers = false;
+var isSymbols = false;
+var password = '';
+
+//Reset to default state
+function reset() {
+  length = '';
+  isLower = false;
+  isUpper = false;
+  isNumbers = false;
+  isSymbols = false;
+  password = '';
+  writePassword();
+}
+//Password criteria selector & password generation
+function writePassword() {
+  var length = prompt("Enter password length! Between 8-128.");
+  length = parseInt(length);
+  if (isNaN(length)) {
+    alert("The input is not a number! Try again!");
+    reset();
+    return;
+  } else if (length < 8) {
+    alert("Password to short needs to be longer than 8.");
+    reset();
+    return;
+  } else if (length > 128) {
+    alert("Password to long needs to be shorter than 128.");
+    reset();
+    return;
+  }
+  var isLower = confirm('Would you like to include lower case letters?');
+  var isUpper = confirm('Would you like to include upper case letters?');
+  var isNumbers = confirm('Would you like to include numbers?');
+  var isSymbols = confirm('Would you like to include symbols?');
+  if (isLower === false && isUpper === false && isNumbers === false && isSymbols === false) {
+    alert('You need to select at least one option!');
+    reset();
+  }
+  password = '';
+  var randSelected = '';
+  for (var i = 0; i < length; i++) {
+    randSelected = randomFunc(isLower, isUpper, isNumbers, isSymbols);
+    if (randSelected !== "retry") {
+      password = password + randSelected;
+    } else {
+      i--;
+    }
+  }
+  console.log(password);
+  document.getElementById("password").innerHTML = password;
+  return password;
+}
+//Select random characters for password generation
+function randomFunc(isLower = false, isUpper = false, isNumbers = false, isSymbols = false) {
+  var selector = Math.floor(Math.random() * 4);
+  if (isLower && selector === 0) {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  } else if (selector === 0) {
+    return "retry";
+  }
+  if (isUpper && selector === 1) {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  } else if (selector === 1) {
+    return "retry";
+  }
+  if (isNumbers && selector === 2) {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  } else if (selector === 2) {
+    return "retry";
+  }
+  if (isSymbols && selector === 3) {
+    const symbols = '!@#$%^&*'
+    return symbols[Math.floor(Math.random() * symbols.length)];
+  } else if (selector === 3) {
+    return "retry";
+  }
+  console.log(isLower, isUpper, isNumbers, isSymbols);
+};
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
